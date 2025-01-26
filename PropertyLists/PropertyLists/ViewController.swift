cb0e45f3b0bcd175cede77bd3573c90bd9573707
Master Moviles <mastermoviles@MacBook-Air.local>
2025-01-26 18:19:29 +0100

    ejercicio2

---
+++ PropertyLists/PropertyLists/ViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    struct Alumno : Codable {
+        var nombre : String
+        var notas : [Float]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let a1 = Alumno(nombre: "Pepe", notas: [5, 8.5, 10])
+        let a2 = Alumno(nombre: "Eva", notas: [10, 9])
+        let alumnos = [a1,a2]
+        
+        var urlDocs = FileManager.default.urls(for:.documentDirectory,
+                                               in:.userDomainMask)[0]
+        let urlPlist = urlDocs.appendingPathComponent("alumnos.plist")
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .xml
+        
+        do {
+            let data = try encoder.encode(alumnos)
+            try data.write(to: urlPlist)
+        } catch {
+            
+        }
+    }
+}
+
